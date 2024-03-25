fc3ccf31fecc5c1f7e545cac359309aea89dd9bd
manzaero <ligoero@gmail.com>
2024-03-25 23:48:41 +0300

    refact from css, fetch, imports & testing in postman

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+    const [posts, setPosts] = useState([]);
+    const [currentPage, setCurrentPage]  = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [fetching, setFetching] = useState(false);
+
+
+
+    useEffect(()=> {
+
+        const fetchRequest = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+            const postData = await response.json()
+            console.log(postData)
+        }
+        fetchRequest()
+    }, [])
   return (
-    <div className="App">
-      <h2>Hello</h2>
+    <div className="container">
+      <h2>
+          <div className="title">
+              title
+          </div>
+          <p className="post">
+              post
+          </p>
+      </h2>
     </div>
   );
 }
